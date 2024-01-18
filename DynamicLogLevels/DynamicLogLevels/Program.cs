3fce8bd0b20fa8363d652fe7d16e2ad5fc186e28
Osman ORHAN <osmnorhn@gmail.com>
2024-01-18 16:34:40 +0100

    Initial implementation of Dynamic Log Levels

--- DynamicLogLevels/DynamicLogLevels/Program.cs
+++ DynamicLogLevels/DynamicLogLevels/Program.cs
@@ -1,54 +1,22 @@
 using System;
-using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Hosting;
 using Serilog;
 
 var builder = WebApplication.CreateBuilder(args);
-builder.Configuration.AddJsonFile(path: "config/logSettings.json", optional: true, reloadOnChange: true);
+builder.Configuration.AddJsonFile(path: "config/logSettings.json", optional: false, reloadOnChange: true)
+    .AddEnvironmentVariables();
 
 // Add services to the container.
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
 
 var log = new LoggerConfiguration()
     .ReadFrom.Configuration(builder.Configuration)
     .CreateLogger();
 
 var app = builder.Build();
-
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
-
-app.UseHttpsRedirection();
-
-var summaries = new[]
-{
-    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-};
-
-app.MapGet("/weatherforecast", () =>
-    {
-        var forecast = Enumerable.Range(1, 5).Select(index =>
-                new WeatherForecast
-                (
-                    DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
-                    Random.Shared.Next(-20, 55),
-                    summaries[Random.Shared.Next(summaries.Length)]
-                ))
-            .ToArray();
-        return forecast;
-    })
-    .WithName("GetWeatherForecast")
-    .WithOpenApi();
 
 Task.Run(async () =>
 {
@@ -67,8 +35,3 @@ Task.Run(async () =>
 });
 
 app.Run();
-
-record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
-{
-    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
-}
--- DynamicLogLevels/DynamicLogLevels/run.ps1
+++ DynamicLogLevels/DynamicLogLevels/run.ps1
@@ -1,5 +1,5 @@
 docker build -f Dockerfile -t dynamic-logs:latest $PSScriptRoot/../
 
 kubectl config use-context docker-desktop --namespace default
-kubectl apply -f $PSScriptRoot/k8s/cm.yaml
+kubectl apply -f $PSScriptRoot/k8s/configmap.yaml
 kubectl apply -f $PSScriptRoot/k8s/deployment.yaml
